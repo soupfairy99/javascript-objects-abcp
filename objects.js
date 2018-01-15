59a82d32074cd128ee7c0d5d956a67a21831dc45
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-15 17:03:14 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,50 @@
+var playlist = {
+ 'Slowdive':'Alison','Sometimes':'My Bloody Valentine'
+   }
+  
+  function updatePlaylist(playlist,artistName,songTitle) 
+  {
+    playlist[artistName] = songTitle
+    return playlist
+  }
+  
+ updatePlaylist(playlist,'Phil Ochs',"Here's to the State of Mississippi")  ;
+ 
+
+  }
+  function removeFromPlaylist(playlist,artistName){delete["Slowdive"]; return removeFromPlaylist}
+    
+/*function updatePlaylist(playlist,artist,song) {playlist.assign({'Alison':'Slowdive'},{'Sometimes':'My Bloody Valentine'})return updateplay
+
+it('defines an object called `playlist` containing at least one artist-song pair', () => {
+    expect(typeof playlist).to.equal('object')
+    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  })
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
+  describe('removeFromPlaylist(playlist, artistName)', () => {
+    it('removes `artistName` from `playlist`', () => {
+      removeFromPlaylist(playlist, 'Slowdive')
+
+      expect(playlist).
+        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+
+      expect(playlist).
+        not.to.have.all.keys({'Slowdive': 'Alison'})
+    })
+  })
+})
+*/
